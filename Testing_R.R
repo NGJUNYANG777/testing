b392fea2e0af4a43fee93602eff1f6967bfe4998
NGJUNYANG777 <80335332+NGJUNYANG777@users.noreply.github.com>
2021-03-10 12:00:49 +0800

    Update Testing_R.R
    summary of linking GitHub with r studio

--- Testing_R.R
+++ Testing_R.R
@@ -1,3 +1,5 @@
 print("This file was created within RStudio")
 
 print("And now it lives on GitHub")
+#In this lesson, we linked Git and RStudio, so that RStudio recognizes you are using Git as your version control software. Following that, we linked RStudio to GitHub, so that you can push and pull repositories from within RStudio. To test this, we created a repository on GitHub, linked it with a new project within RStudio, created a new file, and then staged, committed, and pushed the file to your GitHub repository!
+
